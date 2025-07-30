6dfc2521e1a25ddd85b71cc7825f35379ae18951
Tohirov Muhammadsodiq <220481@centralasian.uz>
2025-07-30 14:14:38 +0500

    Hamma fayllar

---
+++ atla-forum/src/components/BenderStatistics.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    props: ['characters'],
+    computed: {
+        benderStatistics() {
+            const elements = ['Air', 'Earth', 'Fire', 'Water'];
+            const statistics = {
+                Air: 0,
+                Earth: 0,
+                Water: 0,
+                Fire: 0,
+            };
+
+            this.characters.forEach((character) => {
+                elements.forEach((element) => {
+                    if (character.element.includes(element)) {
+                        statistics[element] += 1;
+                    }
+                });
+            });
+
+            return statistics;
+        },
+    },
+};
+</script>
+
+<template>
+    <h2>Statistics</h2>
+    <ul>
+        <li v-for="(stat, type) in benderStatistics" :key="`bender-${stat}-${type}`">
+            {{ type }}: {{ stat * 2 }}
+        </li>
+    </ul>
+</template>
---
+++ playground/src/components/user-card.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>User: {{ user.name }}</h1>
+    <p>Favourite Food: {{ user.food }}</p>
+    <button @click="tellParentChangeName">Change Name</button>
+</template>
+<script>
+export default {
+    props: {
+        user: {
+            type: Object,
+            required: true
+        },
+    },
+    methods: {
+        tellParentChangeName() {
+            this.$emit('change-name');
+        }
+    }
+
+}
+</script>
